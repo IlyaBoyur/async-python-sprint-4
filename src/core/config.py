179dccf5e17dabca933776c4fa8194417ff6125a
Ilya Boyur <i.boyur@idaproject.com>
2023-04-24 12:56:22 +0300

    Add fastapi core files

---
+++ src/core/config.py
@@ -0,0 +1,27 @@
+import os
+from logging import config as logging_config
+
+from pydantic import BaseSettings, PostgresDsn
+
+from core.logger import LOGGING
+
+
+logging_config.dictConfig(LOGGING)
+
+
+PROJECT_NAME = os.getenv("PROJECT_NAME", "URL Shortener App")
+PROJECT_HOST = os.getenv("PROJECT_HOST", "127.0.0.1")
+PROJECT_PORT = int(os.getenv("PROJECT_PORT", "8080"))
+PROJECT_DB = os.getenv(
+    "PROJECT_DB",
+    "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
+)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class AppSettings(BaseSettings):
+    app_title: str = PROJECT_NAME
+    database_dsn: PostgresDsn = PROJECT_DB
+
+
+app_settings = AppSettings()
