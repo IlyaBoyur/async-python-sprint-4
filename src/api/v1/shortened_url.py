07a9eb0b410b949bf6878a501e6e7a4f30be4000
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-04-25 21:00:03 +0300

    Add API to read URL usage statistics

--- src/api/v1/shortened_url.py
+++ src/api/v1/shortened_url.py
@@ -1,12 +1,12 @@
 import logging
-from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.db import get_session
 from schemas import shortened_url as short_url_schema
-from services.services import shortened_url_service as short_url_service
+from schemas.short_url_use import ShortURLUseRead, ShortURLUseReadCut
+from services.services import short_url_service, url_use_service
 from services.shortener import generate_short_url
 
 router = APIRouter()
@@ -25,13 +25,34 @@ async def read_short_url(
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="URL is not found"
         )
-    headers = {"Location": url_object.original}
     logger.info(f"Redirecting to: {url_object.original}")
+    headers = {"Location": url_object.original}
     return Response(
         content="",
         status_code=status.HTTP_307_TEMPORARY_REDIRECT,
         headers=headers,
     )
+
+
+@router.get("/{id}/status")
+async def read_short_url_statistics(
+    *,
+    db: AsyncSession = Depends(get_session),
+    id: int,
+    full_info: bool | None = None,
+    offset: int = 0,
+    max_result: int = 10,
+) -> list[ShortURLUseRead] | ShortURLUseReadCut:
+    """Get URL statistics by ID"""
+    if not full_info:
+        url_uses_count = await url_use_service.count(
+            db, filter=dict(url_id=id)
+        )
+        return ShortURLUseReadCut(count=url_uses_count)
+    url_uses = await url_use_service.get_multi(
+        db=db, filter=dict(url_id=id), skip=offset, limit=max_result
+    )
+    return url_uses
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
