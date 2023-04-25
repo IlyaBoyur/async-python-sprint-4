4bdfb1ae570d4fd65e1ffce560b7138f5a32fcdc
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-04-26 02:00:57 +0300

    Add URL use logging

--- src/api/v1/shortened_url.py
+++ src/api/v1/shortened_url.py
@@ -1,32 +1,87 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    Header,
+    HTTPException,
+    Request,
+    Response,
+    status,
+)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.db import get_session
 from schemas import shortened_url as short_url_schema
-from schemas.short_url_use import ShortURLUseRead, ShortURLUseReadCut
+from schemas.short_url_use import (
+    ShortURLUseCreate,
+    ShortURLUseRead,
+    ShortURLUseReadCut,
+)
+from schemas.user import UserRead
 from services.services import short_url_service, url_use_service
 from services.shortener import generate_short_url
+
+from .user import get_user
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.get("/{id}")
-async def read_short_url(
+def host_extractor(request: Request) -> str:
+    return request.client.host
+
+
+def port_extractor(request: Request) -> int:
+    return request.client.port
+
+
+async def get_short_url(
     *,
     db: AsyncSession = Depends(get_session),
     id: int,
-) -> Response:
-    """Get URL by ID"""
+) -> short_url_schema.ShortenedURLRead:
     url_object = await short_url_service.get(db=db, id=id)
     if url_object is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="URL is not found"
         )
-    logger.info(f"Redirecting to: {url_object.original}")
-    headers = {"Location": url_object.original}
+    return url_object
+
+
+async def log_url_use(
+    *,
+    db: AsyncSession = Depends(get_session),
+    host: str = Depends(host_extractor),
+    port: int = Depends(port_extractor),
+    user_agent: str | None = Header(default=None),
+    user: UserRead | None = Depends(get_user),
+    short_url: short_url_schema.ShortenedURLRead = Depends(get_short_url),
+) -> ShortURLUseRead:
+    url_use_data = ShortURLUseCreate.parse_obj(
+        {
+            "host": host,
+            "port": port,
+            "user_agent": user_agent or "unknown",
+            "url_id": short_url.id,
+            "user_id": getattr(user, "id", None),
+        }
+    )
+    db_object = await url_use_service.create(db=db, object_in=url_use_data)
+    use = ShortURLUseRead.from_orm(db_object)
+    logger.info(f"Logged URL use: {use}")
+    return use
+
+
+@router.get("/{id}")
+async def read_short_url(
+    *,
+    short_url: short_url_schema.ShortenedURLRead = Depends(get_short_url),
+    use: ShortURLUseRead = Depends(log_url_use),
+) -> Response:
+    """Get URL by ID & log use"""
+    logger.info(f"Redirecting to: {short_url.original}")
+    headers = {"Location": short_url.original}
     return Response(
         content="",
         status_code=status.HTTP_307_TEMPORARY_REDIRECT,
