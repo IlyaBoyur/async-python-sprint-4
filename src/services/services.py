d0a77cac8b10bd0f663e0f6122c248f45270644f
Ilya Boyur <i.boyur@idaproject.com>
2023-04-25 20:59:40 +0300

    Add short URL use service

--- src/services/services.py
+++ src/services/services.py
@@ -1,4 +1,6 @@
 from models.models import ShortenedURL as ShortenedURLModel
+from models.models import ShortenedURLUse as ShortenedURLUseModel
+from schemas.short_url_use import ShortURLUseCreate
 from schemas.shortened_url import ShortenedURLCreate
 
 from .base import RepositoryDB
@@ -10,4 +12,13 @@ class RepositoryShortenedURL(
     pass
 
 
-shortened_url_service = RepositoryShortenedURL(ShortenedURLModel)
+short_url_service = RepositoryShortenedURL(ShortenedURLModel)
+
+
+class RepositoryShortenedURLUse(
+    RepositoryDB[ShortenedURLUseModel, ShortURLUseCreate, None]
+):
+    pass
+
+
+url_use_service = RepositoryShortenedURLUse(ShortenedURLUseModel)
