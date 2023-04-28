e1996e9ff262a37ddb2cc75b52e21052db4ef876
Ilya Boyur <i.boyur@idaproject.com>
2023-04-28 22:26:17 +0300

    Add db time check routes

--- src/api/v1/base.py
+++ src/api/v1/base.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from .shortened_url import router
+from .db import router as db_router
+from .shortened_url import router as short_url_router
 
 api_router = APIRouter()
 
-api_router.include_router(router, prefix="")
+api_router.include_router(db_router, prefix="")
+api_router.include_router(short_url_router, prefix="")
