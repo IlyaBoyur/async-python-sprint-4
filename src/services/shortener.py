2a2a1f20389ee5200804a09e8a49a02c8d9b0a27
Ilya Boyur <i.boyur@idaproject.com>
2023-04-30 18:47:48 +0300

    Aggregate all project settings in pydantic BaseSettings object

--- src/services/shortener.py
+++ src/services/shortener.py
@@ -2,13 +2,15 @@ import logging
 
 import pyshorteners
 
-from core.config import PROJECT_SHORTENER
+from core.config import app_settings
 
 logger = logging.getLogger(__name__)
 
 
 def generate_short_url(original: str) -> str:
-    shortener = getattr(pyshorteners.Shortener(), PROJECT_SHORTENER, None)
+    shortener = getattr(
+        pyshorteners.Shortener(), app_settings.project_shortener, None
+    )
     if shortener is None:
         raise RuntimeError("Shortener is not configured or incorrect")
     short = shortener.short(url=original)
